738cc76be351a15f3bfb76ebce802e09cf35f80c
Vlad Filippov <vlad.filippov@gmail.com>
2016-06-08 13:52:15 -0400

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# PWAify
+
+> Convert your PWA (Progressive Web App) into a cross-platform Electron App.
+
+
+## Usage
+
+> Node 4+ required.
+
+Install:
+
+```
+npm install -g pwaify
+```
+
+## Run against your PWA app
+
+```
+pwaify https://airhorner.com
+```
+
+Open the app on your platform, test and send it to your friends!
+
+## Changelog
+
+* 1.0.0 - First experimental release
---
+++ index.js
@@ -0,0 +1,47 @@
+const debug = require('debug')('index');
+const packager = require('electron-packager');
+const fs = require('fs');
+const chalk = require('chalk');
+const path = require('path');
+const manifest = require('./lib/manifest');
+
+module.exports = function (options) {
+  options = options || {};
+  debug('options', options);
+  const appUrl = options.appUrl;
+  return manifest.fetchManifestDetails(appUrl)
+    .then(function (manifestJson) {
+      debug('manifestJson', manifestJson);
+      var name = manifestJson.name || manifestJson.short_name;
+      var start_url = appUrl + manifestJson.start_url ;
+      var appData = {
+        appUrl: start_url
+      };
+      fs.writeFileSync(path.join(__dirname, 'template', 'prefs.json'), JSON.stringify(appData));
+      var packagerOpts = {
+        name: name,
+        arch: 'all',
+        overwrite: true,
+        dir: path.join(__dirname, 'template'),
+        platform: options.platforms || 'all'
+      };
+
+      return new Promise(function (resolve) {
+        packager(packagerOpts, function done_callback(err, appPaths) {
+          if (err) {
+            throw err;
+          }
+
+          resolve({
+            appPaths: appPaths
+          })
+        })
+      })
+
+    })
+    .then(function (result) {
+      console.log(chalk.green('Your app is ready!'), result);
+    }).catch(function (err) {
+      throw err;
+    })
+};
