e56fffb984cac7433faed65211a6881a7c9ea6ba
getive <getive@126.com>
2017-12-14 09:51:01 +0800

    Update index.js
    Added support for CLI --manifest option.

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ module.exports = function (options) {
   debug('options', options);
   const appUrl = options.appUrl;
   const appPath = options.path;
-
-  return manifest.fetchManifestDetails(appUrl)
+  const manifestUrl = options.manifestUrl;
+  return manifest.fetchManifestDetails(appUrl, manifestUrl)
     .then(function(manifestJson) {
       debug('manifestJson', manifestJson);
       var name = manifestJson.name || manifestJson.short_name;
